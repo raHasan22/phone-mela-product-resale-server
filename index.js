9253044f1428191b29d3234a5f0eb19010c038ac
raHasan22 <r.ahmad.hasan.r@gmail.com>
2022-11-28 15:23:34 +0600

    two operation added

--- index.js
+++ index.js
@@ -13,11 +13,28 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8uogbca.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+    try{
+        const phoneInfoCollection = client.db('resellService').collection('phoneInfo');
+        const bookingCollection = client.db('resellService').collection('bookingInfo');
+
+        app.get('/phoneInfoCollection', async(req, res) => {
+            const query = {};
+            const phoneInfo = await phoneInfoCollection.find(query).toArray();
+            res.send(phoneInfo);
+        })
+
+        app.post('/bookings', async(req, res) =>{
+            const booking = req.body
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 
 app.get('/', async(req, res) =>{
